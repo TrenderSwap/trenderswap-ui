6364a66cb94c61c3ed7e971947d9e0c3437a5b82
boldman <aj.bold14@gmail.com>
2021-07-03 14:01:43 +0100

    Alot of lib repair

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
-import { ChainId, JSBI, Percent, Token, WETH } from '@pancakeswap-libs/sdk'
+import { ChainId, JSBI, Percent, Token, WETH } from '@boldrack/sdk'
 
 export const ROUTER_ADDRESS = '0xAF066Ce38a7aAEdf6481f5aa5C459221e04a58F1'
 
