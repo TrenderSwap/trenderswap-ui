43dddc4ec04d522280cafe6b678be61111e3d558
boldman <aj.bold14@gmail.com>
2021-07-03 15:06:27 +0100

    Phew!

--- src/utils/wrappedCurrency.ts
+++ src/utils/wrappedCurrency.ts
@@ -1,4 +1,4 @@
-simport { ChainId, Currency, CurrencyAmount, ETHER, Token, TokenAmount, WETH } from '@boldrack/sdk'
+import { ChainId, Currency, CurrencyAmount, ETHER, Token, TokenAmount, WETH } from '@boldrack/sdk'
 
 export function wrappedCurrency(currency: Currency | undefined, chainId: ChainId | undefined): Token | undefined {
   // eslint-disable-next-line no-nested-ternary
